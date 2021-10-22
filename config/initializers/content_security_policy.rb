fc2ab74501ad6018dcc243fcddcda4f8333b5eea
BIG-BEN-Coder <blazerof@uccs.edu>
2021-10-22 22:12:14 +0000

    Added Google Analytics Work Around

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -23,3 +23,8 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+Rails.application.config.content_security_policy do |policy|
+    policy.script_src :self, 'https://www.googletagmanager.com', 'https://www.google-analytics.com'
+  end
+
